63c3523209d88846bf99c4c04a4f4442eae20101
myteams-hitomgr <oubo@my-teams.co.jp>
2026-06-24 17:58:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,15 @@
 const { chromium } = require('playwright');
 
-const accounts = [
-  {
-    id: process.env.HITOMGR_ID_A,
-    pass: process.env.HITOMGR_PASSWORD,
-    name: "A"
-  },
-  {
-    id: process.env.HITOMGR_ID_B,
-    pass: process.env.HITOMGR_PASSWORD,
-    name: "B"
-  }
-];
-
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false }); 
+  const page = await browser.newPage();
 
-  for (const acc of accounts) {
-    const page = await browser.newPage();
+  await page.goto('https://kanri.hitomgr.jp/lwf3/login');
 
-    console.log(`ログイン開始: ${acc.name}`);
+  console.log("ログイン画面表示");
 
-    await page.goto('https://kanri.hitomgr.jp/lwf3/login');
-
-    await page.waitForTimeout(3000);
-
-    await page.close();
-
-    console.log(`完了: ${acc.name}`);
-  }
+  // ここで少し待つ（重要）
+  await page.waitForTimeout(5000);
 
   await browser.close();
 })();
